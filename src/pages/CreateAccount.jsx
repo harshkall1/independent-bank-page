aa5c4da752c92a3c03994dcea1e34ca1eae3e154
9678anu <anupriyavarshney9678@gmail.com>
2025-03-22 17:23:13 +0530

    data

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -13,6 +13,8 @@ const CreateAccount = () => {
         phoneNo: "",
         password: "",
         amount: 0,
+        bankname: "Independent Bank"
+
     });
 
     // State for loading
@@ -51,6 +53,8 @@ const CreateAccount = () => {
                 phoneNo: "",
                 password: "",
                 amount: 0,
+                bankname: "Independent Bank"
+
             });
 
             // Navigate to a confirmation page
